71b51af9a9a01828c82b14585447bef9f08b4073
Varshil-Shah <varshilashah09@gmail.com>
2022-06-21 15:26:23 +0530

    :sparkles: Build progress bar container
    Add progress bar container and add functionality to update progress bar and percentage

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const dropZone = document.querySelector('.drop-zone');
 const fileInput = document.querySelector('#fileInput');
 const browseButton = document.querySelector('.browseButton');
 
+const progressContainer = document.querySelector('.progress-container');
+const bgProgress = document.querySelector('.bg-progress');
+const percent = document.querySelector('#percent');
+const progressBar = document.querySelector('.progress-bar');
+const progressTitle = document.querySelector('.progress-title');
+
 const host = 'https://innshare.herokuapp.com';
 const uploadUrl = `https://reqres.in/api/users`;
 
@@ -30,11 +36,17 @@ browseButton.addEventListener('click', (e) => {
 });
 
 const updateProgress = (e) => {
-  const percentage = Math.round((e.loaded / e.total) * 100);
-  console.log(percentage);
+  const percentage = Math.floor((e.loaded / e.total) * 100);
+  bgProgress.style.width = `${percentage + 0.5}%`;
+  progressBar.style.width = `${percentage + 0.5}%`;
+  if (percentage === 100) {
+    progressTitle.textContent = 'Uploaded successfully';
+  }
+  percent.textContent = percentage;
 };
 
 const uploadFile = () => {
+  progressContainer.style.display = 'block';
   const file = fileInput.files[0];
   const formData = new FormData();
   formData.append('myfile', file);
@@ -43,6 +55,7 @@ const uploadFile = () => {
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       console.log(xhr.response);
+      showLink({ file: 'File Name', link: 'https://google.com/api' });
     }
   };
 
@@ -50,6 +63,11 @@ const uploadFile = () => {
 
   xhr.open('POST', uploadUrl, true);
   xhr.send(formData);
+};
+
+const showLink = ({ link }) => {
+  progressContainer.style.display = 'none';
+  console.log(link);
 };
 
 fileInput.addEventListener('change', uploadFile);
